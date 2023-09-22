4e343f875d94b999e093350b8622dbb2004a2077
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-22 15:50:11 +0300

    Add bootstrap to project

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './BookStore.css';
-import Home from './Home';
+import Homepage from './Home';
 import Categories from './Categories';
 
 function BookStore() {
   return (
+
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={Home} />
-          <Route path="/categories" element={Categories} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
       </div>
     </BrowserRouter>
-
   );
 }
 
