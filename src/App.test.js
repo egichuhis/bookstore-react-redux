63990b81f86433a7426b422c096162c92324dbbf
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-25 17:47:37 +0300

    Refactor code to use 'app.js' from 'bookstore.js'

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import BookStore from './components/BookStore';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<BookStore />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import BookStore from './components/BookStore';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BookStore />
+    <App />
   </React.StrictMode>,
 );
